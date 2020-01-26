865a4a100f945538195df4d9f12eeb924c463c56
Patrick A <akolo@hotmail.com>
2020-01-26 11:49:01 +0000

    Automatically backed up by Learn

--- spec/02_challenge_spec.rb
+++ spec/02_challenge_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 require_relative '../lib/second_challenge.rb'
 
 describe "second_challenge" do
-	xit "fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs" do
+	it "fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs" do
     expect(second_challenge[:montague].keys).to eq([:patriarch, :matriarch, :hero, :hero_friends])
     expect(second_challenge[:capulet].keys).to eq([:patriarch, :matriarch, :heroine, :heroine_friends])
     expect(second_challenge[:montague].values).to eq([{}, {}, {}, []])

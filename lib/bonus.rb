2927060ccea74714d9b81b672f89a48654938c88
Patrick A <akolo@hotmail.com>
2020-01-26 12:10:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
 epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulate][:heroine][:status] = "dead"
+epic_tragedy[:capulat][:heroine][:status] = "dead"
 puts epic_tragedy
   
 

46ea9019a2a7ab0c0f9a6457bf0902b8098f99ee
Patrick A <akolo@hotmail.com>
2020-01-26 11:47:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,32 +1,6 @@
 def first_challenge
    epic_tragedy = {
     :montague => {},
-    :capulet => {
-       :patriach => {
-         :name => "Lord Capulet",
-         :age => "50"
-       },
-       :matriach => {
-         :name => "Lady Capulet",
-         :age => "51"
-       },
-       :heroine => {
-         :name =>"Juliet",
-         :age => "15",
-         :status => 'alive'
-       },
-       :heroine_friends => [
-         {
-         :name => "Steve",
-         :age =>"30",
-         :attitude => "confused"
-         },
-         {
-         :name => "Nurse",
-         :age => "44",
-         :attitude => "worried"
-         }
-         ]
-    }
+    :capulet => {}
    }
 end

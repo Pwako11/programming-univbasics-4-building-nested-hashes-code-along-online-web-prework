05929c62c8d5b19436aff6232a866552b48a9157
Patrick A <akolo@hotmail.com>
2020-01-26 11:48:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -27,7 +27,32 @@ def second_challenge
          ]
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
+       :patriach => {
+         :name => "Lord Capulet",
+         :age => "50"
+       },
+       :matriach => {
+         :name => "Lady Capulet",
+         :age => "51"
+       },
+       :heroine => {
+         :name =>"Juliet",
+         :age => "15",
+         :status => 'alive'
+       },
+       :heroine_friends => [
+         {
+         :name => "Steve",
+         :age =>"30",
+         :attitude => "confused"
+         },
+         {
+         :name => "Nurse",
+         :age => "44",
+         :attitude => "worried"
+         }
+         ]
       }}
 
   

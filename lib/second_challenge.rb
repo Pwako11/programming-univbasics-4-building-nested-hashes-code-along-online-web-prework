55769197359fd5288feec27dd9d48d9c7174ef8e
Patrick A <akolo@hotmail.com>
2020-01-26 11:56:26 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,37 +1,15 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriach => {
-         :name => "Lord Montague",
-        :age => "53"
-      },
-       :matriach => {
-         :name => "Lady Montague",
-         :age => "54"
-      },
-       :hero => {
-          :name => "Romeo",
-          :age => "15",
-          :status => "alive"
-       },
-       :hero_friends => [
-         
-         ]
+    :montague => {
+       :patriach => {},
+       :matriach => {},
+       :hero => {},
+       :hero_friends => []
       }, 
     :capulet => {
-      
-       :patriach => {
-         :name => "Lord Capulet",
-         :age => "50"
-       },
-       :matriach => {
-         :name => "Lady Capulet",
-         :age => "51"
-       },
-       :heroine => {
-         :name =>"Juliet",
-         :age => "15",
-         :status => 'alive'
-       },
+       :patriach => {},
+       :matriach => {},
+       :heroine => {},
        :heroine_friends => []
       }
     
